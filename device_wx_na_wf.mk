c8aef342971d91b516ad9920bcb392af16bac13f
C457 <android.c357@gmail.com>
2014-08-14 17:31:17 -0500

    Clean up for only cwm

--- device_wx_na_wf.mk
+++ device_wx_na_wf.mk
@@ -11,11 +11,6 @@ DEVICE_PACKAGE_OVERLAYS += device/nvidia/wx_na_wf/overlay
 
 LOCAL_PATH := device/nvidia/wx_na_wf
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init.recovery.ardbeg.rc:root/init.recovery.ardbeg.rc \
-    $(LOCAL_PATH)/twrp.fstab:recovery/root/etc/twrp.fstab \
-    $(LOCAL_PATH)/ueventd.ardbeg.rc:recovery/root/ueventd.ardbeg.rc
-
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
