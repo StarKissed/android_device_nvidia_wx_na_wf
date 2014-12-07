cb4002bd2499d0f1c99a344a8a6cb587002e747e
TwistedUmbrella <twistedumbrella@gmail.com>
2014-12-07 10:42:46 -0500

    kernel: temporarily use external source prebuilt

--- device_wx_na_wf.mk
+++ device_wx_na_wf.mk
@@ -11,6 +11,25 @@ DEVICE_PACKAGE_OVERLAYS += device/nvidia/wx_na_wf/overlay
 
 LOCAL_PATH := device/nvidia/wx_na_wf
 
+# Prebuilt Kernel
+ifneq ($(OUT),)
+ifeq ($(MK_KERNEL_OBJ),)
+$(shell rm -rf $(OUT)/obj/KERNEL_OBJ;\
+    mkdir -p $(OUT)/obj;\
+    ln -s $(ANDROID_BUILD_TOP)/$(LOCAL_PATH)/include/KERNEL_OBJ $(OUT)/obj/KERNEL_OBJ)
+MK_KERNEL_OBJ := true
+endif
+endif
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
