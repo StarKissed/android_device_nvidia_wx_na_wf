3ea408bc914307a8558f0e9cf9029a93ad18b315
TwistedUmbrella <twistedumbrella@gmail.com>
2014-12-09 11:31:22 -0500

    package: update config, establish release package

--- device_wx_na_wf.mk
+++ device_wx_na_wf.mk
@@ -5,30 +5,30 @@ LOCAL_PATH := device/nvidia/wx_na_wf
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/nvidia/wx_na_wf/wx_na_wf-vendor.mk)
+$(call inherit-product-if-exists, vendor/nvidia/shieldtablet/shieldtablet-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/nvidia/wx_na_wf/overlay
 
 LOCAL_PATH := device/nvidia/wx_na_wf
 
 # Prebuilt Kernel
-ifneq ($(OUT),)
-ifeq ($(MK_KERNEL_OBJ),)
-$(shell rm -rf $(OUT)/obj/KERNEL_OBJ;\
-    mkdir -p $(OUT)/obj;\
-    ln -s $(ANDROID_BUILD_TOP)/$(LOCAL_PATH)/include/KERNEL_OBJ $(OUT)/obj/KERNEL_OBJ)
-MK_KERNEL_OBJ := true
-endif
-endif
+#ifneq ($(OUT),)
+#ifeq ($(MK_KERNEL_OBJ),)
+#$(shell rm -rf $(OUT)/obj/KERNEL_OBJ;\
+#    mkdir -p $(OUT)/obj;\
+#    ln -s $(ANDROID_BUILD_TOP)/$(LOCAL_PATH)/include/KERNEL_OBJ $(OUT)/obj/KERNEL_OBJ)
+#MK_KERNEL_OBJ := true
+#endif
+#endif
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
+#ifeq ($(TARGET_PREBUILT_KERNEL),)
+#	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+#else
+#	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+#endif
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_KERNEL):kernel
 
 $(call inherit-product, build/target/product/full.mk)
 
