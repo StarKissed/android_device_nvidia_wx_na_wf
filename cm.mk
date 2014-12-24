b108e175edd3fb3961ac7fda5467573ad2014ede
TwistedUmbrella <twistedumbrella@gmail.com>
2014-12-24 10:29:01 -0500

    recovery: update build signature override to 5.0.1

--- cm.mk
+++ cm.mk
@@ -12,10 +12,9 @@ $(call inherit-product, device/nvidia/wx_na_wf/device_wx_na_wf.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=wx_na_wf \
-    TARGET_DEVICE=shieldtablet
-#PRODUCT_BUILD_PROP_OVERRIDES += \
-#    BUILD_FINGERPRINT="nvidia/wx_na_wf/shieldtablet:4.4.2/KOT49H/22229_440.9583:user/release-keys" \
-#    PRIVATE_BUILD_DESC="wx_na_wf-user 4.4.2 KOT49H 22229_440.9583 release-keys"
+    TARGET_DEVICE=shieldtablet \
+    BUILD_FINGERPRINT="nvidia/wx_na_wf/shieldtablet:5.0.1/LRX22C/29082_493.9700:user/release-keys" \
+    PRIVATE_BUILD_DESC="wx_na_wf-user 5.0.1 LRX22C 29082_493.9700 release-keys"
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := wx_na_wf
