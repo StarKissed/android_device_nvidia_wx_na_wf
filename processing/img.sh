5ed6f8d7771192bd19899731cac4f50d322b4589
TwistedUmbrella <twistedumbrella@gmail.com>
2015-01-01 23:21:32 -0500

    build: update directory to mirror upstream output

--- processing/img.sh
+++ processing/img.sh
@@ -4,5 +4,5 @@ else
     BUILDSTRUCT=darwin
 fi
 
-$BUILDSTRUCT/./mkbootfs recovery.img-ramdisk | gzip > ramdisk-recovery.cpio.gz
+$BUILDSTRUCT/./mkbootfs ramdisk-recovery | gzip > ramdisk-recovery.cpio.gz
 $BUILDSTRUCT/./mkbootimg --cmdline 'androidboot.selinux=permissive' --kernel kernel --ramdisk ramdisk-recovery.cpio.gz -o recovery.img --base 0x10000000 --pagesize 2048 --ramdisk_offset 0x11000000 --tags_offset 0x10f00000
